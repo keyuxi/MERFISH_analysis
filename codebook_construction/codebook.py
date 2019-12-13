b66b3271cf4564b5ff89c66a9c641da51d3a9ec9
keyuxi <keyuxi@yeah.net>
2019-12-13 15:38:12 -0800

    Implement transcript selection

--- codebook_construction/codebook.py
+++ codebook_construction/codebook.py
@@ -9,8 +9,7 @@ import pybiomart as bm
 import pandas as pd
 import numpy as np
 import requests, sys
-import json
-import tqdm
+from tqdm import tqdm
 
 class Codebook(object):
     def __init__(self, gene_list_file, codebook_name='',
@@ -82,12 +81,44 @@ class Codebook(object):
         # Retrieve information
         query_result = dataset.query(attributes=attr, filters = filters)
         
-        # Filter out protein coding entries without an Appris annotation
-        query_result[]
+        #####################################################
+        ##### Find the transcript to use for each gene. #####
+        #####################################################
         
-        return query_result
+        # Create a new column for the chosen transcript
+        self.gene_list['ensembl_transcript_id_version'] = ''
+        
+        # For lncRNA, choose the longest transcript.
+        lnc_qr = query_result[query_result['Gene type'] == 'lncRNA']
+        lnc_ind = lnc_qr.groupby(['Gene stable ID']) \
+                ['Transcript length (including UTRs and CDS)'].idxmax()
+        lnc_qr = query_result.loc[lnc_ind]
+        for ind,row in lnc_qr.iterrows():
+            self.gene_list.at[self.gene_list['ensembl_gene_id'] == row['Gene stable ID'],
+                              'ensembl_transcript_id_version'] = row.loc['Transcript stable ID version']
+            if self.verbose:
+                print('\n%s' % self.gene_list[self.gene_list['ensembl_gene_id'] == row['Gene stable ID']])        
+        
+        # For protein coding genes, select the one with smallest APPRIS annotation
+        prot_qr = query_result[query_result['Gene type'] == 'protein_coding']
+        prot_qr['appris_rank'] = prot_qr['APPRIS annotation'].apply(appris2rank)
+        # lowest APPRIS rank
+        prot_ind = prot_qr.groupby(['Gene stable ID'])['appris_rank'].idxmin()
+        prot_qr = query_result.loc[prot_ind]
+        # longest
+        prot_ind = prot_qr.groupby(['Gene stable ID'])['Transcript length (including UTRs and CDS)'].idxmax()
+        prot_qr = query_result.loc[prot_ind]
+        for ind,row in prot_qr.iterrows():
+            self.gene_list.at[self.gene_list['ensembl_gene_id'] == row['Gene stable ID'],
+                              'ensembl_transcript_id_version'] = row.loc['Transcript stable ID version']
+            self.gene_list.at[self.gene_list['ensembl_gene_id'] == row['Gene stable ID'],
+                              'appris_rank'] = row.loc['APPRIS annotation']
+            self.gene_list.at[self.gene_list['ensembl_gene_id'] == row['Gene stable ID'],
+                              'transcript_length'] = row.loc['Transcript length (including UTRs and CDS)']
+        return self.gene_list
 
-
+    def get_abundance(self, abundance_file):
+        pass
     
     def assign_smELT(self):
         # create a new column by copying
@@ -96,9 +127,23 @@ class Codebook(object):
     def assign_barcode(self):
         pass
     
+def appris2rank(anno):
+    """
+    Converts APPRIS annotation to a number for comparison.
+    Returns:
+        r - int.
+    """
+    if not isinstance(anno, str):
+        r = 99
+    elif 'principal' in anno:
+        r = int(anno[-1])
+    elif 'alternative' in anno:
+        r = int(anno[-1]) + 9
+    else:
+        r = 99
+    return r
 
 if __name__ == "__main__":
-    #%%
     gene_list_file = r"C:\Users\Yuxi\workspace\probedesign\gene_list\FISH_markers.txt"
     # readout_file = r"C:\Users\Yuxi\workspace\genomeData\CommonOligos\ReadoutSeqs.fasta"
     out_file = r"C:\Users\Yuxi\workspace\probedesign\gene_list\codebook_BLA.csv"
